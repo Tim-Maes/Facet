21b2ceb9eb5c3dee3d6939eec7d17e9dd8d9e9f5
Tim Maes <maestim@cronos.be>
2025-04-30 09:48:38 +0200

    Cleanup usings

--- src/Facet/Generators/FacetKindGenerator.cs
+++ src/Facet/Generators/FacetKindGenerator.cs
@@ -1,16 +1,14 @@
-using System;
-using System.Linq;
-using System.Reflection;
-using Facet.Util;
 using Microsoft.CodeAnalysis;
 
 namespace Facet.Generators;
 
 [Generator]
-public class FacetKindGenerator : IIncrementalGenerator {
-
-    public void Initialize(IncrementalGeneratorInitializationContext context) {
-        context.RegisterPostInitializationOutput(spc => {
+public class FacetKindGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(spc => 
+        {
             spc.AddSource(
                 hintName: "FacetKind.g.cs",
                 source:
