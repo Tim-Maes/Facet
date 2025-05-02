09a1a80b6449fb1b35554213b3d6a56c0c6d0fc5
Tim Maes <maestim@cronos.be>
2025-05-02 10:45:41 +0200

    Test console app

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using Facet;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Person
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
+
+[Facet(typeof(Person), Exclude = new[] { "Name" }, IncludeFields = true, GenerateConstructor = true)]
+public partial class PersonDto
+{
+
+}
