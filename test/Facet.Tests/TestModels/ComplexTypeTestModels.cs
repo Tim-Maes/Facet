6eb8982137c2e2e099ee7196bf795db06864df15
Tim Maes <91606949+Tim-Maes@users.noreply.github.com>
2025-10-14 21:24:12 +0200

    Tests

--- test/Facet.Tests/TestModels/ComplexTypeTestModels.cs
+++ test/Facet.Tests/TestModels/ComplexTypeTestModels.cs
@@ -44,11 +44,11 @@ public class DepartmentEntity
 [Facet(typeof(AddressEntity))]
 public partial record AddressFacet;
 
-[Facet(typeof(CompanyEntity), NestedFacets = new[] { typeof(AddressFacet) })]
+[Facet(typeof(CompanyEntity), NestedFacets = [typeof(AddressFacet)])]
 public partial record CompanyFacet;
 
-[Facet(typeof(StaffMember), exclude: ["PasswordHash", "Salary"], NestedFacets = new[] { typeof(CompanyFacet), typeof(AddressFacet) })]
+[Facet(typeof(StaffMember), exclude: ["PasswordHash", "Salary"], NestedFacets = [typeof(CompanyFacet), typeof(AddressFacet)])]
 public partial record StaffMemberFacet;
 
-[Facet(typeof(DepartmentEntity), NestedFacets = new[] { typeof(CompanyFacet), typeof(StaffMemberFacet) })]
+[Facet(typeof(DepartmentEntity), NestedFacets = [typeof(CompanyFacet), typeof(StaffMemberFacet)])]
 public partial record DepartmentFacet;
