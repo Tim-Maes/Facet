d8f70cecb8c9ff86c4caef70469dade4020046ee
Tim Maes <91606949+Tim-Maes@users.noreply.github.com>
2025-11-27 16:57:35 +0100

    Extract attributes

--- src/Facet.Attributes/FacetAttribute.cs
+++ src/Facet.Attributes/FacetAttribute.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Facet;
 
@@ -38,7 +38,7 @@ public sealed class FacetAttribute : Attribute
 
     /// <summary>
     /// Whether to generate a parameterless constructor for easier unit testing and object initialization.
-    /// When true, a public parameterless constructor will be generated. For record types without existing 
+    /// When true, a public parameterless constructor will be generated. For record types without existing
     /// primary constructors, this creates a standard class-like parameterless constructor. For positional records,
     /// the parameterless constructor initializes properties with default values.
     /// </summary>
@@ -140,7 +140,7 @@ public sealed class FacetAttribute : Attribute
     /// An array of flattened types that can be generated from this facet's collection properties.
     /// When specified, the generator will create FlattenTo() methods that unpack collection properties
     /// into multiple rows of the specified type, combining parent properties with each collection item.
-    /// The target types should be classes/records that define the properties you want from both 
+    /// The target types should be classes/records that define the properties you want from both
     /// the parent and the collection items.
     /// </summary>
     /// <remarks>
@@ -148,34 +148,34 @@ public sealed class FacetAttribute : Attribute
     /// Example usage:
     /// <code>
     /// // Define the entities
-    /// public class Data 
-    /// { 
+    /// public class Data
+    /// {
     ///     public int Id { get; set; }
     ///     public string Name { get; set; }
     ///     public ICollection&lt;Extended&gt; Extended { get; set; }
     /// }
-    /// 
-    /// public class Extended 
-    /// { 
+    ///
+    /// public class Extended
+    /// {
     ///     public int Id { get; set; }
     ///     public string Name { get; set; }
     ///     public int DataValue { get; set; }
     /// }
-    /// 
+    ///
     /// // Define the facets
     /// [Facet(typeof(Extended))]
     /// public partial class ExtendedDto;
-    /// 
+    ///
     /// [Facet(typeof(Data), NestedFacets = [typeof(ExtendedDto)], FlattenTo = [typeof(DataFlattened)])]
     /// public partial class DataDto;
-    /// 
+    ///
     /// // Define the flattened target (manually specify all properties you want)
     /// public partial class DataFlattened
     /// {
     ///     // From Data (parent)
     ///     public int Id { get; set; }
     ///     public string Name { get; set; }
-    ///     
+    ///
     ///     // From Extended (collection item) - prefix to avoid Name collision
     ///     public string ExtendedName { get; set; }
     ///     public int DataValue { get; set; }
